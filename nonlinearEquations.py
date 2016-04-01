f3cf551a6805ae65d2dee914b8defe024a4b0bf7
iansbartlett <iansbartlett@users.noreply.github.com>
2016-04-01 22:00:38 +1100

    Including existing scratch files.

---
+++ nonlinearEquations.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 31 21:20:01 2016
+
+@author: dinah
+
+Solving nonlinear equations for MATH2089
+"""
+
+#Bisection method- let's go
+#This seems stupid. Probably define a single class- nonlinear_eqn
+#Maybe two- nonlinear_eqn, nonlinear_system
+#Then have the solver algorithms be methods within the class
+
+class nonlinear_eqn:
+
+
+    def __init__(self, function=None):
+        pass        
+
+    def bisection(self):
+        """
+        Solves the problem using the bisection method.
+        
+        Parameters:
+        -Function that we need to obtain the zero for
+        -Upper starting bound
+        -Lower starting bound
+        -Solution tolerance
+        
+        -Solve- obtain zero, number of errors until zero
+    
+        """
+    
+        pass
+    
+    def fixed_point(self):
+        """
+        Solves the problem using the bisection method.
+        
+        Parameters:
+        -Function that we need to obtain the zero for
+        -Upper starting bound
+        -Lower starting bound
+        -Solution tolerance
+        
+        -Solve- obtain zero, number of errors until zero
+    
+        """
+    
+        pass
+    
+    def newton_rhapson(self):
+        pass
+    
+    def secant(self):
+        pass
+    
+    
+class nonlinear_eqn_system:
+
+    """
+    Describes a system of nonlinear equations that we want to solve.
+    """
+
+    
+    def bisection:
+        """
+        Solves the problem using the bisection method.
+        
+        Parameters:
+        -Function that we need to obtain the zero for
+        -Upper starting bound
+        -Lower starting bound
+        -Solution tolerance
+        
+        -Solve- obtain zero, number of errors until zero
+    
+        """
+    
+        pass
+    
+    def fixed_point:
+        """
+        Solves the problem using the bisection method.
+        
+        Parameters:
+        -Function that we need to obtain the zero for
+        -Upper starting bound
+        -Lower starting bound
+        -Solution tolerance
+        
+        -Solve- obtain zero, number of errors until zero
+    
+        """
+    
+        pass
+    
+    def newton_rhapson:
+        pass
+    
+    def secant:
+        pass
+    
---
+++ test_nonlinearEquations.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 31 22:04:41 2016
+
+@author: dinah
+"""
+import nonlinearEquations as nonlin
+
+import numpy as np
+import scipy as sp
+
+def test_nonlinear_Equations(tol=1e-8, printFlag=False):
+    
+    test_bisection_methods(tol,printFlag)
+    pass    
+
+
+#Test single-function functions
+
+def test_bisection(testFunctions, ranges,tol, printFlag):
+    """
+    Test my bisection method by comparing with the output of the scipy function.
+    
+    If printFlag is set to True, print both fuction outputs and the difference.
+    
+    Returns True if the difference is less than tol, False otherwise
+    """
+    
+    for i in range(len(testFunctions)):
+        scipyValue = sp.optimize.bisect(testFunctions[i],ranges[i,0],ranges[i,1])
+        nonlinValue =
+    pass
+
+def test_fixed_point(testFunctions, tol, printFlag):
+    """
+    Test my fixed point method by comparing with the output of the scipy function.
+    
+    If printFlag is set to True, print both fuction outputs and the difference.
+    
+    Returns True if the difference is less than tol, False otherwise
+    """    
+    pass
+
+def test_newton_rhapson(testFunctions, tol, printFlag):
+    """
+    Test my Newton-Rhapson method by comparing with the output of the scipy function.
+    
+    If printFlag is set to True, print both fuction outputs and the difference.
+    
+    Returns True if the difference is less than tol, False otherwise
+    """    
+    pass
+
+def test_secant(testFunctions, tol, printFlag):
+    """
+    Test my secant method by comparing with the output of the scipy function.
+    
+    If printFlag is set to True, print both fuction outputs and the difference.
+    
+    Returns True if the difference is less than tol, False otherwise
+    """    
+    pass
+
+#Test system functions
+
+
+def test_bisection_system(testFunctions,tol, printFlag):
+    """
+    Test my bisection method by comparing with the output of the scipy function.
+    
+    If printFlag is set to True, print both fuction outputs and the difference.
+    
+    Returns True if the difference is less than tol, False otherwise
+    """
+    pass
+
+def test_fixed_point_system(testFunctions, tol, printFlag):
+    """
+    Test my fixed point method by comparing with the output of the scipy function.
+    
+    If printFlag is set to True, print both fuction outputs and the difference.
+    
+    Returns True if the difference is less than tol, False otherwise
+    """    
+    pass
+
+def test_newton_rhapson_system(testFunctions, tol, printFlag):
+    """
+    Test my Newton-Rhapson method by comparing with the output of the scipy function.
+    
+    If printFlag is set to True, print both fuction outputs and the difference.
+    
+    Returns True if the difference is less than tol, False otherwise
+    """    
+    pass
+
+def test_secant_system(testFunctions, tol, printFlag):
+    """
+    Test my secant method by comparing with the output of the scipy function.
+    
+    If printFlag is set to True, print both fuction outputs and the difference.
+    
+    Returns True if the difference is less than tol, False otherwise
+    """    
+    pass
